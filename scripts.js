ef13bcd834850fb00cffefb6f23350af4e9e804a
Mateo <matthew27garcia@gmail.com>
2022-02-24 19:24:38 -0600

    fixed the missing key to function

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ const boxes = document.querySelectorAll('.box')
 
 let running = false
 
-function getRandomColor () {
+function getRandomColor() {
     let letters = "0123456789ABDCDEF"
     let color = '#'
-    for (let i = 0; i <6; i++) {
-        color += letter[Math.floor(Math.random() * 16)]
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
     }
     return color
 }
